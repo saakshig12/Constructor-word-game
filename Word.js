e0d412378a335b1a15ad56193ba4ebeacf460d36
Saakshig12 <saakshig12@gmail.com>
2019-07-30 20:43:26 -0400

    word options added and file links

---
+++ Word.js
@@ -0,0 +1,1 @@
+var letter = require('./Letter.js');
---
+++ index.js
@@ -0,0 +1,18 @@
+var inquirer = require("inquirer");
+var Word = require("./Word.js")
+
+var wordOptions = [
+    growth,
+    economy,
+    ethiopia,
+    nurse,
+    clive,
+    allier,
+    overgeneralization,
+    slack
+];
+
+var guesses = 8;
+var correct = [];
+var letters = false;
+
