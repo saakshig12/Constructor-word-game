d1e5d309adbcd7940748442b31983492e6170b98
Saakshig12 <saakshig12@gmail.com>
2019-07-30 21:07:34 -0400

    word.js file content added but needs work

--- Word.js
+++ Word.js
@@ -1,1 +1,24 @@
 var letter = require('./Letter.js');
+
+function Word() {
+    this.wordValue = [];
+
+    this.checkCharValue = function getword (
+        var getword = getword;
+        for (i = 0; i < this.wordValue.length; i++) {
+            this.wordValue[i] = new Letter(this.wordValue[i]);
+            this.wordValue[i].showCharacter();
+        }
+
+        this.splitWord = function () {
+            var stringValue = ""
+            this.singleLetters.forEach(item = {
+
+                stringValue = stringValue + item.checkChar() + " "
+
+            })
+            console.log(stringValue)
+        }
+    }
+
+    module.exports = Word;
